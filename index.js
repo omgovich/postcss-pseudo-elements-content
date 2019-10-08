c4e79e0d6108c39c6a014f5eed92bde8e0c6e002
Vlad Shilov <omgovich@ya.ru>
2019-10-08 19:45:49 +0300

    PostCSS v7 support

--- index.js
+++ index.js
@@ -1,13 +1,23 @@
 var postcss = require('postcss')
 
-module.exports = postcss.plugin('postcss-pseudo-elements-content', function (opts) {
-  opts = opts || {}
+var customPseudoExp = /(.*::?)(after|before)(-c)$/
 
-  // Work with options here
+module.exports = postcss.plugin('postcss-pseudo-elements-content', function () {
+  return function (root) {
+    root.walkRules(function (rule) {
+      var hasCustomPseudo = false
 
-  return function (root, result) {
+      // Process selectors
+      rule.selectors = rule.selectors.map(function (selector) {
+        if (customPseudoExp.test(selector)) {
+          hasCustomPseudo = true
+          return selector.replace(customPseudoExp, '$1$2')
+        }
+        return selector
+      })
 
-    // Transform CSS AST here
-
+      // Add `content: ''` declaraion
+      if (hasCustomPseudo) rule.append({ prop: 'content', value: "''" })
+    })
   }
 })
--- index.test.js
+++ index.test.js
@@ -1,18 +1,20 @@
 var postcss = require('postcss')
+var fs = require('fs')
 
 var plugin = require('./')
 
 function run (input, output, opts) {
-  return postcss([plugin(opts)]).process(input).then(function (result) {
-    expect(result.css).toEqual(output)
-    expect(result.warnings()).toHaveLength(0)
-  })
+  return postcss([plugin(opts)])
+    .process(input, { from: undefined })
+    .then(function (result) {
+      expect(result.css).toEqual(output)
+      expect(result.warnings()).toHaveLength(0)
+    })
 }
 
-/* Write tests here
+it('adds content property', function () {
+  var input = fs.readFileSync('./test/input.css', 'utf-8')
+  var expected = fs.readFileSync('./test/expected.css', 'utf-8')
 
-it('does something', function () {
-  return run('a{ }', 'a{ }', { })
+  return run(input, expected, {})
 })
-
-*/
